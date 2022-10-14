d2e4c799500edbd4491c94ab87ff4fd8edb0d3d5
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-10-14 19:02:43 +0530

    starting server port 8080

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+
+
+const app=express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.get('/',(req,res)=>res.send('Hello'));
+
+
+
+app.listen(8080,()=>{
+console.log('server started port 8080');
+});
