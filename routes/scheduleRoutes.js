5d8930fe3fbdaa9db7f96699ff1e20154d33e3de
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-10-15 15:15:02 +0530

    applying routes to the scheduler

---
+++ routes/scheduleRoutes.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const { newSchedule, getSchedule, updateSchedule, deleteSchedule } = require("../controllers/schedul̥eController");
+const { isAuthenticatedUser } = require("../middleware/auth");
+
+const router = express.Router();
+
+router.post("/schedule",newSchedule);
+router.get("/schedule",getSchedule);
+router.put("/schedule/:id",isAuthenticatedUser,updateSchedule);
+router.delete("/schedule/:id",isAuthenticatedUser,deleteSchedule);
+
+module.exports=router;
