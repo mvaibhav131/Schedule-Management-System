ba37aab6f2bef5e484d5c12ee4b366d27f94657e
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-10-15 13:23:54 +0530

    managing user routes

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const cors= require("cors");
 const connection = require("./database");
 const cookieParser= require("cookie-parser");
 const bodyParser = require('body-parser');
+const userroute= require("./routes/userRoute");
 
 // set up server
 
 const app=express();
-
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -20,11 +20,14 @@ app.use(cookieParser());
 // set up the body parser for handling form data
 // submitted by the user
 app.use(bodyParser.urlencoded({ extended: false }));
+// set up the session
+
 
 // set up path of config
 dotenv.config({path:'config/config.env'});
 
 // set up routes
+app.use("/api/v1",userroute);
 
 app.get('/',(req,res)=>res.send('Hello'));
 
---
+++ routes/userRoute.js
@@ -0,0 +1,12 @@
+
+const express =require("express");
+const { registerUser, loginUser, logout } = require("../controllers/userController");
+
+
+const router= express.Router();
+
+router.post("/register",registerUser);
+router.post("/login",loginUser);
+router.get("/logout",logout);
+
+module.exports=router;
