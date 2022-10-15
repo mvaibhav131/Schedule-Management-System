5a52ffeaed64b5191a9b0146fadfc295413a0f6a
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-10-15 10:32:04 +0530

    Connecting server to database

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express=require('express');
+const dotenv= require("dotenv");
+const cors= require("cors");
+const connection = require("./database");
+const cookieParser= require("cookie-parser");
 
 
 const app=express();
 
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cookieParser());
 
+//config
+dotenv.config({path:'config/config.env'});
 
+// Routes
 app.get('/',(req,res)=>res.send('Hello'));
 
 
 
-app.listen(8080,()=>{
-console.log('server started port 8080');
+app.listen(process.env.PORT,async()=>{
+    try{
+        await connection();
+        console.log("connected to MongoDB");
+    }
+    catch(e){
+        console.log("error",e);
+    }
+console.log(`server started port ${process.env.PORT} `);
 });
